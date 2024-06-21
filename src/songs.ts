6c5b72fef1e41431d8dba0849e7d01692777fa60
Guillem Blasco <gowet92@gmail.com>
2024-06-21 20:27:33 +0200

    Finish task

--- src/songs.ts
+++ src/songs.ts
@@ -1,10 +1,8 @@
 export const isTitleEmpty = (songTitle: string): boolean => {
-  let isEmpty: boolean;
-  // Asígnale a la variable isEmpty un true o un false dependiendo de si
-  // la variable songTitle recibida tiene un string vacío o no
-
-  isEmpty = true; // Cambia esta línea por tu código
-
+  let isEmpty: boolean = false;
+  if (songTitle === "") {
+    isEmpty = true;
+  }
   return isEmpty;
 };
 
@@ -12,49 +10,44 @@ export const doesTitleExist = (
   songTitle: string,
   songTitles: string[]
 ): boolean => {
-  let doesExist: boolean;
-  // Asígnale a la variable doesExist un true o un false dependiendo de si
-  // la variable title recibida ya existe en el array de títulos recibido
+  let doesExist: boolean = false;
 
-  doesExist = true; // Cambia esta línea por tu código
+  if (songTitles.includes(songTitle)) {
+    doesExist = true;
+  }
 
   return doesExist;
 };
 
 export const isTitleShort = (songTitle: string): boolean => {
-  let isShort: boolean;
-  // Asígnale a la variable isShort un true o un false dependiendo de si
-  // la variable title recibida tiene menos de 3 caracteres
-
-  isShort = true; // Cambia esta línea por tu código
-
+  let isShort: boolean = false;
+  if (songTitle.length < 3) {
+    isShort = true;
+  }
   return isShort;
 };
 
 export const isPlaylistFull = (songTitles: string[]): boolean => {
-  let isFull: boolean;
-  // Asígnale a la variable isFull un true o un false dependiendo de si
-  // el array de títulos recibido tiene 5 o más elementos
-
-  isFull = true; // Cambia esta línea por tu código
-
+  let isFull: boolean = false;
+  if (songTitles.length >= 5) {
+    isFull = true;
+  }
   return isFull;
 };
 
 export const addSong = (songTitle: string, songTitles: string[]): void => {
-  // Añade la variable title al array de títulos recibido
+  songTitles.push(songTitle);
 };
 
 export const sortSongs = (songTitles: string[]): void => {
-  // Ordena alfabéticamente el array de títulos recibido
+  songTitles = songTitles.sort();
 };
 
 export const getSongsCount = (songTitles: string[]): number => {
-  let songsCount: number;
-  // Asígnale a la variable songsCount el número de elementos del array recibido
-
-  songsCount = 0; // Cambia esta línea por tu código
-
+  let songsCount: number = 0;
+  for (let song = 0; song < songTitles.length; song++) {
+    songsCount++;
+  }
   return songsCount;
 };
 
@@ -62,22 +55,25 @@ export const removeSongByPosition = (
   songTitles: string[],
   position: number
 ): void => {
-  // Elimina del array de títulos recibido el elemento que se encuentra en la posición recibida
+  songTitles = songTitles.splice(position);
 };
 
 export const getErrorMessage = (errorCode: string): string => {
   let errorMessage: string;
 
-  // A la variable anterior le tendrás que asignar un mensaje de error
-  // dependiendo del código de error recibido
-  // Si el código de error no es válido, asígnale un mensaje genérico
-  // Éstos son los códigos de error y sus mensajes correspondientes:
-  // - "required": "No has introducido ningún título"
-  // - "exists": "La canción ya existe"
-  // - "too-short": "El título es demasiado corto"
-  // - "limit": "La playlist está llena"
-
-  errorMessage = ""; // Cambia esta línea por tu código
-
-  return errorMessage;
+  switch (errorCode) {
+    case "required":
+      errorMessage = "No has introducido ningún título";
+      return errorMessage;
+    case "exists":
+      errorMessage = "La canción ya existe";
+      return errorMessage;
+    case "too-short":
+      errorMessage = "El título es demasiado corto";
+      return errorMessage;
+    case "limit":
+      errorMessage = "La playlist está llena";
+      return errorMessage;
+  }
+  return "";
 };
